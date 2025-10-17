9034c332ccc89e5a391de6d73584082d3f93556c
Friktata <capitanofriktata@gmail.com>
2025-10-17 05:21:24 +0100

    Almost...

--- src/js/config/Depmanager.config.js
+++ src/js/config/Depmanager.config.js
@@ -9,8 +9,8 @@
     console.log(`HOST = ${window.location.hostname}`)
 
     if (window.location.hostname !== "127.0.0.1" && window.location.hostname !== "localhost") {
-        builtins_path = "https://github.com/Friktata/friktata/tree/master/src/js/core/PolyBASIC/builtins";
-        plugins_path = "https://github.com/Friktata/friktata/tree/master/src/js/PolyBASIC_plugins";
+        builtins_path = "https://raw.githubusercontent.com/Friktata/friktata/master/src/js/core/PolyBASIC/builtins";
+        plugins_path = "https://raw.githubusercontent.com/Friktata/friktata/master/src/js/PolyBASIC_plugins";
     }
 
     export const DepmanagerConfig = (() => {
